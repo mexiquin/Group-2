ed9ace8eda056a8879134bae205d4406ee96bdf7
Quinton Jasper <dropatuningmetal@gmail.com>
2020-09-11 08:54:31 -0700

    Revert "Update Code.java"

--- assignmentPR/Code.java
+++ assignmentPR/Code.java
@@ -44,81 +44,80 @@ class Code {
         return true;
     }
 
-    static String F1(String numString) {
-      // Declaring/Initializing variables
-      String numerator = "";
-      String denominator = "10";
-      String mantissa;
-      String after_dec;
-      int num_of_zeros = 0;
-      int i;
-      int characteristic;
-
-      // Checking results for functions
-      if (characteristic(numString) && mantissa(numString)) {
-          // Checking the number of decimals
-          if ((numString.split("\\.")).length == 2) {
-              after_dec = numString.split("\\.")[1];
-
-              for (i = 0; i < after_dec.length(); i++) {
-
-                // Counting number of zeros after decimal but before first non-zero number
-                if (after_dec.charAt(i) == '0') {
-                    num_of_zeros++;
-                }
-
-                if (after_dec.charAt(i) != '0') {
-                    numerator = after_dec.substring(after_dec.indexOf(after_dec.charAt(i)));
-                    break;
-                }
-              }
-
-              // If no number after decimal, mantissa is 0
-              if (numerator == "") {
-                  mantissa = "0";
-              }
-
-              // else, add zeros to denominator based off of length of number
-              else {
-                  for (i = 0; i < num_of_zeros; i++) {
-                      denominator += "0";
-                  }
-
-                  for (i = 1; i < numerator.length(); i++) {
-                      denominator += "0";
-                  }
-
-                  // Get rid of leading/trailing whitespace
-                  numerator = numerator.trim();
-                  // Concatenate to create a "fraction"
-                  mantissa = numerator + '/' + denominator;
-              }
-          }
-
-          // Check if no decimals
-          else {
-              mantissa = "0";
-          }
-
-          // Initializing the characteristic
-          characteristic = (int)Double.parseDouble(numString);
-
-          return "Characteristic: " + Integer.toString(characteristic) + " Mantissa: " + mantissa;
-
-      }
-
-      // Number isn't valid
-      else {
-          return "The input number is not valid";
-      }
-    }
-
     public static void main(String[] args) {
 
         // Test number
         String number = "23.44602";
 
-        System.out.println(F1(number));
+        // Declaring/Initializing variables
+        String numerator = "";
+        String denominator = "10";
+        String mantissa;
+        String after_dec;
+        int num_of_zeros = 0;
+        int i;
+        int characteristic;
+
+        // Checking results for functions
+        if (characteristic(number) && mantissa(number)) {
+            // Checking the number of decimals
+            if ((number.split("\\.")).length == 2) {
+                after_dec = number.split("\\.")[1];
+
+                for (i = 0; i < after_dec.length(); i++) {
+
+                  // Counting number of zeros after decimal but before first non-zero number
+                  if (after_dec.charAt(i) == '0') {
+                      num_of_zeros++;
+                  }
+
+                  if (after_dec.charAt(i) != '0') {
+                      numerator = after_dec.substring(after_dec.indexOf(after_dec.charAt(i)));
+                      break;
+                  }
+                }
+
+                // If no number after decimal, mantissa is 0
+                if (numerator == "") {
+                    mantissa = "0";
+                }
+
+                // else, add zeros to denominator based off of length of number
+                else {
+                    for (i = 0; i < num_of_zeros; i++) {
+                        denominator += "0";
+                    }
+
+                    for (i = 1; i < numerator.length(); i++) {
+                        denominator += "0";
+                    }
+
+                    // Get rid of leading/trailing whitespace
+                    numerator = numerator.trim();
+                    // Concatenate to create a "fraction"
+                    mantissa = numerator + '/' + denominator;
+                }
+            }
+
+            // Check if no decimals
+            else {
+                mantissa = "0";
+            }
+
+            // Initializing the characteristic
+            characteristic = (int)Double.parseDouble(number);
+
+            // Printing out results
+            System.out.println("Characteristic: " + characteristic);
+
+            System.out.println("Mantissa: " + mantissa);
+
+        }
+
+        // Number isn't valid
+        else {
+            System.out.println("The input number is not valid");
+        }
 
     }
 }
